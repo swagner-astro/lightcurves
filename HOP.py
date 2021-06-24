d93680d34eb3f2ebb734ed08b2a305744b58b620
swagner <s.wagner@shiftee.eu>
2021-06-24 19:04:17 +0200

    useful comments

--- HOP.py
+++ HOP.py
@@ -1,20 +1,29 @@
-'''
-HOP Class - Sarah Wagner
-========================
-
-Takes start, peak and end time of a flare (flux variation) as determined with the HOP algorithm (see LC_new.py)
-
-'''
-
 import numpy as np
 from matplotlib import pyplot as plt
 from selfmade_py.LC_new import LightCurve
 
-class Hopject:
 
-    def __init__(self, hop_params, lc):
+
+
+class Hopject: 
+    '''
+    HOP Class
+    ==========
+    A segment in a light curve, i.e. a group of Bayesian blocks, that represent a flare (also called HOP group)
+    For definition of start, peak and end of the flare check out the LightCurve class
+    '''
+    def __init__(self, hop_params, lc): #e.g. Hopject(lc.get_hop_method[0], lc)
         self.start_time, self.peak_time, self.end_time = hop_params
         self.lc = lc
+        #self.mask = which bins -> mask to have all HOP bins of lc
+        #self.n_bins = how many bins
+        #self.coverage = bins/time
+        self.n_blocks = lc.bb_i_end(self.end_time) - lc.bb_i_start(self.start_time) #e.g. one-block hop: 5 - 3 = 2 
+        if lc.z:
+            self.z = lc.z
+        else:
+            self.z = np.nan
+
         self.dur = self.end_time - self.start_time
         self.rise_time = self.peak_time - self.start_time
         self.decay_time = self. end_time - self. peak_time
@@ -25,19 +34,12 @@ class Hopject:
         self.end_flux = lc.block_val[lc.bb_i_end(self.end_time)]
         self.rise_flux = self.peak_flux - self.start_flux
         self.decay_flux = self.peak_flux - self.end_flux
-        self.n_blocks = lc.bb_i_end(self.end_time) - lc.bb_i_start(self.start_time)
-        # eg one-block hop: 5 - 3 = 2 
-        if lc.z:
-            self.z = lc.z
-        else:
-            self.z = np.nan
 
-        # how many bins; coverage = bins/time e.g. = 1 for daily binning
-        # which bins -> mask to then fit and so on
-
-# exponential flare function se my_forschung/coding
-
+    #--------------------------------------------------------------------------------------------------------------------------------
     def plot_hop(self):
+        """
+        Plot the snip of light curve with this flare
+        """
         self.lc.plot_bblocks()
         plt.xlim(self.start_time - self.dur/2, self.end_time + self.dur/2)
         x = np.linspace(self.start_time, self.end_time)
