8dc278c946078277d3be98c4713301126b802fc8
swagner-astro <sarahwagner1602@googlemail.com>
2022-01-13 19:43:02 +0100

    add on for multiwavelength light curves

--- MLC.py
+++ MLC.py
@@ -52,7 +52,7 @@ class MultiLC:
         lc_id = list of strings identifying each LC, eg ['Fermi', 'XRT', ...]
         """
         self.lc_list = np.array(lc_list)
-        self.lc_id = lc_id 
+        self.lc_id = np.array(lc_id) 
         if len(lc_list) != len(lc_id):
             raise ValueError('LightCurves do not match identifiers')
         self.name = str(name)
@@ -75,6 +75,25 @@ class MultiLC:
                 lc.plot_lc(**kwargs)
             plt.ylabel(lc_id, fontsize=15)
         plt.xlabel('Time [MJD]', fontsize=15)
+
+    #-----------------------------------------------------------------------------------------------
+    def insert_lc(self, position, lc, lc_id):
+        """
+        add another light curve
+        make sure to avoid mutable presto-chango with 
+        >>> mlc = copy.deepcopy(mlc_array)
+        >>> mlc.insert_lc()
+        otherwise inserted in every execution!
+        """
+        if position == -1:
+            self.lc_list = np.append(self.lc_list, lc)
+            self.lc_id = np.append(self.lc_id, lc_id)
+            self.n = len(self.lc_list)
+        else:
+            self.lc_list = np.insert(self.lc_list, position, lc)
+            self.lc_id = np.insert(self.lc_id, position, lc_id)
+            self.n = len(self.lc_list)
+        return(self)
 
 
 
